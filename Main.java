6e8f5cd9169dc27631254aab8c78968cc1cf56f9
0liwer <oliwer.suwik@gmail.com>
2024-10-12 16:50:50 +0200

    liczenie

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 public class Main {
 public static void main(String[] args) {
     System.out.println("Hello word");
+    int number = 5;
+
+    number = 3;
+    number -= 10;
+
+    System.out.println(number);
+
+    int a =5;
+    int b =7;
+    int sum = a+b;
+    System.out.println(sum);
+
+    float myFirstNumber = 3.3f;
+    float mySecondNumber = 5.2f;
+    float myResult = myFirstNumber / mySecondNumber;
+
+            System.out.println(myResult);
     }
 }
 
